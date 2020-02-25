9e9d84bf70f8bc64b2116cf6188761d7bf54e906
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-02-25 11:57:11 +0000

    Pre commit hide (#3)
    * Added pre-commit to encrypt secrets
    
    * Added pubkeys
    
    * Resealed for pubkeyring users
    
    * Removed pre-hook, added with pipelines
    
    * Reseal from mac
    
    * Added some instructions
    
    * Readded pubkeys to keyring, resealed secret

---
+++ add-key.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILES=*.asc
+count=`ls -1 $FILES 2>/dev/null | wc -l`
+
+# git clone secret-repo
+# if [[ ! -d git-secret-repo ]]; then
+#     git clone --quiet git@github.com:dwpdigital/git-secret-repo \
+#     && cd git-secret-repo
+# else
+#     cd git-secret-repo
+#     git fetch --quiet
+# fi
+
+shopt -s nullglob
+if [ $count != 0 ]; then
+    for f in $FILES
+    do
+        GPG_EMAIL=$(gpg $f 2> /dev/null | perl -ne 'print "$1\n" if (/<([^>+]+)>/)')
+        gpg --import $f \
+        && git secret tell $GPG_EMAIL
+    done
+fi
